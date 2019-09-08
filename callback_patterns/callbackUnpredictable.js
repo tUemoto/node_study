43a31b5429e0cc6ad0f8787e0171e906d79fd601
tUemoto <takspooh@gmail.com>
2019-09-09 02:14:41 +0900

    * 同期と非同期の混在がある場合の挙動についてコメント追加

--- callback_patterns/callbackUnpredictable.js
+++ callback_patterns/callbackUnpredictable.js
@@ -4,6 +4,8 @@ const cache = {};
 
 /**
  * 同期CPSと非同期CPSの混在
+ * cacheが存在するなら同期関数として働き
+ * cacheが存在しないなら非同期関数として働く
  * @param  {String}   filename [description]
  * @param  {Function} callback [description]
  * @return {[type]}            [description]
@@ -35,9 +37,16 @@ const createFileReader = (filename) => {
 
 const reader1 = createFileReader('data.txt');
 reader1.onDataReady((data) => {
+  // reader1はcacheなしの状態でinconsistentReadが呼ばれるので、
+  // callback関数の実行は非同期で行われる
+  // そのためreader1.onDataReadyの実行よりも後にcallbackが実行され、
+  // 結果としてデータが取得できる。
   console.log('First call data: ', data);
   const reader2 = createFileReader('data.txt');
   reader2.onDataReady((data2) => {
+    // 一方でreader2の時は、cacheにすでに`data.txt`が登録されているため、
+    // 即座にcallback関数が実行される。
+    // そのため、onDataReadyでは何も行われない
     console.log('Second call data: ', data2);
   });
 });
